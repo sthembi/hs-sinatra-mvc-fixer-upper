a431df1cc44ed212cbf104977ae90b9bf71e9ad1
rpremvaree12 <richard.premvaree@gmail.com>
2015-08-11 13:52:00 +0000

    fixed typos in README and model. removed piglatinize.rb

--- application_controller.rb
+++ application_controller.rb
@@ -8,10 +8,10 @@ class ApplicationController < Sinatra::Base
   end
 	
 	post '/transport_results' do # route tells me where to go when action ='/results'
-		user_location = params["location"]     	# save their name
-		user_miles = params["miles"]   					# save their miles
+		user_location = params["location"]     	# save the location
+		user_miles = params["miles"]   					# save the miles
     decision = transportation_decision(user_location, user_miles)  # pass location and miles to method
-    erb :transport_results # this load our transport_results.erb page
+    erb :transport_results # this loads our transport_results.erb page
   end
 
 end
