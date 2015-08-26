0e0707f7ab6925048994bd54bf8adbb40eae50bb
Susan Delmoor-Rodriguez <susan@bfuturedigital.net>
2015-08-26 10:40:09 +0000

    solved it

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,7 @@
 require 'bundler'
 Bundler.require
+#this was missing
+require_relative 'models/model.rb' #<== require models logic
 
 class ApplicationController < Sinatra::Base
 
@@ -8,9 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 	
 	post '/transport_results' do # route tells me where to go when action ='/results'
-		user_location = params["location"]     	# save the location
-		user_miles = params["miles"]   					# save the miles
-    decision = transportation_decision(user_location, user_miles)  # pass location and miles to method
+    # incorrect hash notation
+		user_location = params[:location]     	# save the location
+    user_miles = params[:distance]   					# save the miles  #correct the label for miles
+    # the method name was not correct
+    @decision = transport_decision(user_location, user_miles)  # pass location and miles to method
     erb :transport_results # this loads our transport_results.erb page
   end
 
--- models/model.rb
+++ models/model.rb
@@ -1,7 +1,7 @@
 # Tells which transport mechanism to take
 def transport_decision(location, miles)
   # miles < 3, then pogostick
-  if miles < 3
+  if miles.to_i < 3   #correct convert string to integer
 		"To get to #{location}, you should take a pogo stick!"
   else   # else, then segway
 		"To get to #{location}, you should take a segway!"
